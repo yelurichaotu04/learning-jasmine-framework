7b0abc777de1e41735173c7a72a6ea29c7477453
Yelurichaout04 <Yelurichaout04@gmail.com>
2015-04-04 11:46:02 +0530

    Added calculator.js in src.

---
+++ WorkSpace-Jasmine/jasmine-standalone-2.0.0/src/Calculator.js
@@ -0,0 +1,10 @@
+function Calculator(){
+this.add=function(num1,num2){
+return(num1+num2)};
+this.sub=function(num1,num2){
+return(num1-num2)};
+this.mult=function(num1,num2){
+return(num1*num2)};
+this.div=function(num1,num2){
+return(num1/num2)};
+};
